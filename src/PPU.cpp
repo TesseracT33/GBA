2720af57c467d1ff34fb01826d3d79cf5da0038b
TesseracT33 <christoffer.ekgrim98@hotmail.com>
2022-09-09 08:35:06 +0200

    ppu initialization code

--- src/PPU.cpp
+++ src/PPU.cpp
@@ -130,9 +130,41 @@ namespace PPU
 
 	void Initialize()
 	{
-		//objects.resize(sizeof(ObjectData) * max_objects);
-		//Scanline();
-		//UpdateRotateScalingRegisters();
+		std::memset(&dispcnt, 0, sizeof(dispcnt));
+		std::memset(&green_swap, 0, sizeof(green_swap));
+		std::memset(&dispstat, 0, sizeof(dispstat));
+		std::memset(&v_counter, 0, sizeof(v_counter));
+		for (int i = 0; i < 4; ++i) {
+			std::memset(&bgcnt[i], 0, sizeof(bgcnt[i]));
+			std::memset(&bghofs[i], 0, sizeof(bghofs[i]));
+			std::memset(&bgvofs[i], 0, sizeof(bgvofs[i]));
+		}
+		for (int i = 0; i < 2; ++i) {
+			std::memset(&bgpa[i], 0, sizeof(bgpa[i]));
+			std::memset(&bgpb[i], 0, sizeof(bgpb[i]));
+			std::memset(&bgpc[i], 0, sizeof(bgpc[i]));
+			std::memset(&bgpd[i], 0, sizeof(bgpd[i]));
+			std::memset(&bgx[i], 0, sizeof(bgx[i]));
+			std::memset(&bgy[i], 0, sizeof(bgy[i]));
+			std::memset(&winh_x1[i], 0, sizeof(winh_x1[i]));
+			std::memset(&winh_x2[i], 0, sizeof(winh_x2[i]));
+			std::memset(&winv_x1[i], 0, sizeof(winv_x1[i]));
+			std::memset(&winv_x2[i], 0, sizeof(winv_x2[i]));
+		}
+
+		std::memset(&winin, 0, sizeof(winin));
+		std::memset(&winout, 0, sizeof(winout));
+		std::memset(&mosaic, 0, sizeof(mosaic));
+		std::memset(&bldcnt, 0, sizeof(bldcnt));
+		std::memset(&eva, 0, sizeof(eva));
+		std::memset(&evb, 0, sizeof(evb));
+		std::memset(&evy, 0, sizeof(evy));
+		in_hblank = in_vblank = false;
+		cycle = dot = framebuffer_index = 0;
+		oam.fill(0);
+		palette_ram.fill(0);
+		vram.fill(0);
+		objects.clear();
 	}
 
 
