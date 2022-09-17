50fded26698fd9705a2f20d5c2c8defb009d632e
Christoffer Ekgrim <christoffer.ekgrim98@hotmail.com>
2022-09-17 17:27:33 +0200

    keyinput: 0=pressed, not released

--- src/Keypad.cpp
+++ src/Keypad.cpp
@@ -7,20 +7,21 @@ namespace Keypad
 {
 	void Initialize()
 	{
-		keyinput = keycnt = 0;
+		keyinput = 0x3FF;
+		keycnt = 0;
 	}
 
 
 	void NotifyButtonPressed(uint index)
 	{
-		keyinput |= 1 << index;
+		keyinput &= ~(1 << index);
 		UpdateIrq();
 	}
 
 
 	void NotifyButtonReleased(uint index)
 	{
-		keyinput &= ~(1 << index);
+		keyinput |= 1 << index;
 	}
 
 
@@ -61,12 +62,12 @@ namespace Keypad
 		static constexpr u16 irq_cond_mask = 1 << 15;
 		if (keycnt & irq_enable_mask) {
 			if (keycnt & irq_cond_mask) { /* logical AND mode */
-				if ((keyinput & keycnt & 0x3FF) == keycnt) {
+				if ((~keyinput & keycnt & 0x3FF) == keycnt) {
 					IRQ::Raise(IRQ::Source::Keypad);
 				}
 			}
 			else { /* logical OR mode */
-				if (keyinput & keycnt & 0x3FF) {
+				if (~keyinput & keycnt & 0x3FF) {
 					IRQ::Raise(IRQ::Source::Keypad);
 				}
 			}
